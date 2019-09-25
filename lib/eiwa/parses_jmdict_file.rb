33bd305ce03fe9fb1684d7b05385258c8db9b084
Justin Searls <searls@gmail.com>
2019-09-25 15:47:17 +0900

    Not actually using this (or benefitting from it)

--- lib/eiwa/parses_jmdict_file.rb
+++ lib/eiwa/parses_jmdict_file.rb
@@ -7,7 +7,6 @@ module Eiwa
       JmdictDoc.new(each_entry_block).tap do |doc|
         Nokogiri::XML::SAX::Parser.new(doc).parse_file(filename) do |ctx|
           ctx.recovery = true
-          ctx.replace_entities = true
         end
       end.result
     end
