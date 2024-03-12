7fa819eaa9e0125d1fc92e5270a75d52a2b7d5c2
Justin Searls <searls@gmail.com>
2024-03-12 13:43:30 -0400

    fix test name

--- test/safe/jmdict_e_entity_limit_test.rb
+++ test/safe/jmdict_e_entity_limit_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class JmdictETest < Minitest::Test
-  def test_misc_tag_regression
+  def test_entity_error_limit
     entry = Eiwa.parse_file("test/fixture/jmdict_e_entity_limit.xml").find { |e|
       e.id == 1224880
     }
